1789b74f63251a4304558d0b2817d4092ce189a4
laptop <greenbay.graham@gmail.com>
2018-07-09 21:02:00 +1200

    slow progress

--- hinatore_start.py
+++ hinatore_start.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, flash
 from flask import render_template
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def home():
+    flash('b')
     return render_template('start.html')
+
+@app.route('/answer')
+def answer():
+    return render_template('answer.html')
 
 if __name__ == '__main__':
     # http://flask.pocoo.org/docs/1.0/api/#flask.Flask.run
