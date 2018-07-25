00a26f8d03b6ccf67f3b8abd7aebfcbec3b6aaff
laptop <greenbay.graham@gmail.com>
2018-07-25 17:20:08 +1200

    altered errror logging as the server log is just coming out 1 character per line

--- hinatore_start.py
+++ hinatore_start.py
@@ -16,11 +16,6 @@ dictConfig({
     'formatters': {'default': {
         'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
     }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
     'root': {
         'level': 'INFO',
         'handlers': ['wsgi']
